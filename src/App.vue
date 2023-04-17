25084ccbc449d9ae46e2d1175cde557ed81380da
ans <ans@nts.ch>
2023-04-17 18:16:39 +0200

    updated App.vue code

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
   <v-app>
-    <img alt="Aiza GmbH Logo" src="./assets/aiza_logo.png" width="300" height="168.75" />
+    <img class="logo" alt="Aiza GmbH Logo" src="./assets/aiza_logo.png" width="300" height="168.75" />
     <hr />
-    <v-main>
+    <v-row justify="start">
       <div class="container">
-        <h2>Die Aiza GmbH mit Sitz in Zollikofen ist spezialisiert auf die Entwicklung von massgeschneiderten Webseiten für Unternehmen. Das Hauptaugenmerk liegt auf der Entwicklung von hoch skalierbaren, global effizienten und sicheren Webseiten. Projekterfahrung und fundierte Kenntnisse bestehen in den folgenden Technologien:</h2>
+        <h3>Die Aiza GmbH mit Sitz in Zollikofen ist spezialisiert auf die Entwicklung von massgeschneiderten Webseiten für Unternehmen. Das Hauptaugenmerk liegt auf der Entwicklung von hoch skalierbaren, global effizienten und sicheren Webseiten. Projekterfahrung und fundierte Kenntnisse bestehen in den folgenden Technologien:</h3>
       </div>
-    </v-main>
-    <v-card flex="row" shaped class="rounded-card">
-      <div class="icon-div"><img class="icon" alt="Go Logo" src="./assets/icon_go.png" width="100" height="136"/></div>
-      <div class="icon-div"><img class="icon" alt="Angular Logo" src="./assets/icon_angular.png" width="129" height="136"/></div>
-      <div class="icon-div"><img class="icon" alt="Vue.js Logo" src="./assets/icon_vue.png" width="152" height="130"/></div>
-      <div class="icon-div"><img class="icon" alt="RESTful API Logo" src="./assets/icon_rest.png" width="136" height="136"/></div>
-      <div class="icon-div"><img class="icon" alt="Kubernetes Logo" src="./assets/icon_kubernetes.png" width="136" height="136"/></div>
-      <div class="icon-div"><img class="icon" alt="Gitlab Logo" src="./assets/icon_gitlab.png" width="147" height="136"/></div>
-    </v-card>
-    <v-col class="footer">
-      <v-row justify="center">
-        <v-col cols="auto" class="address">
-          <h2 class="address-text">Aiza GmbH</h2>
-          <h3 class="address-text">Bernstrasse 159</h3>
-          <h3 class="address-text">3052 Zollikofen</h3>
-          <h3 class="address-text">angela.scherer@aiza.ch</h3>
-          <h3 class="address-text">+41 79 197 21 53</h3>
-        </v-col>
-      </v-row>
-    </v-col>
+    </v-row>
+    <v-row justify="center" class="images">
+      <img class="icon" alt="Go Logo" src="./assets/icon_go.png" width="100" height="136"/>
+      <img class="icon" alt="Angular Logo" src="./assets/icon_angular.png" width="129" height="136"/>
+      <img class="icon" alt="Vue.js Logo" src="./assets/icon_vue.png" width="152" height="130"/>
+      <img class="icon" alt="RESTful API Logo" src="./assets/icon_rest.png" width="136" height="136"/>
+      <img class="icon" alt="Kubernetes Logo" src="./assets/icon_kubernetes.png" width="136" height="136"/>
+      <img class="icon" alt="Gitlab Logo" src="./assets/icon_gitlab.png" width="147" height="136"/>
+    </v-row>
+    <v-row justify="start">
+      <div class="container">
+        <h3>Bei Interesse für einen Projektauftrag wenden Sie sich bitte an:</h3><br>
+        <h2 class="address-text">Aiza GmbH</h2>
+        <h3 class="address-text">Bernstrasse 159</h3>
+        <h3 class="address-text">3052 Zollikofen</h3>
+        <h3 class="address-text">angela.scherer@aiza.ch</h3>
+        <h3 class="address-text">+41 79 197 21 53</h3>
+      </div>  
+    </v-row>   
   </v-app>
 </template>
 
 <style lang="sass" scoped>
-v-app 
-  background: white
+
+.logo
+  margin-left: 16px
 
 hr
   border: none
@@ -42,32 +42,19 @@ hr
   margin-right: 16px
   &:before
     height: 8px
-    width: 20%
+    width: 288px
     content: ''
     position: absolute
     background: black !important
 
+.images 
+  margin: -30px 80px
+
 .container
-  margin: 16px !important
-  color: black
-  margin-bottom: 80px !important
-
-.rounded-card
-  border-radius: 200px
-  background: #002775
-
-
-.icon-div
-  width: 150px
+  margin: 60px 80px 20px 80px
 
 .icon 
+  margin-top: 24px
   margin-right: 40px
 
-.address
-  margin: 40px 12px 12px 0
-
-.address-text
-  color: black
-  margin: 0 !important
-  text-decoration: noney
 </style>

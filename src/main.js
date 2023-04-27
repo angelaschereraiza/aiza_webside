b91a05bebab450ba7fba935b79b9b8fa1437db0e
ans <ans@nts.ch>
2023-04-27 16:55:55 +0200

    updated App.vue code

--- src/main.js
+++ src/main.js
@@ -15,10 +15,17 @@ import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
 
+// Gets page language from local storage or browser setting
 if (localStorage.getItem("english") === null) {
-    var userLang = navigator.language || navigator.userLanguage
-    if (userLang.includes("de")) localStorage.setItem("english", false) 
-    else localStorage.setItem("english", true) 
+  var userLang = navigator.language || navigator.userLanguage
+  if (userLang.includes("de")) {
+    localStorage.setItem("english", false) 
+    document.documentElement.setAttribute('lang', 'de')
+  }
+  else {
+    localStorage.setItem("english", true)
+    document.documentElement.setAttribute('lang', 'en')
+  }
 }
 
 registerPlugins(app)
